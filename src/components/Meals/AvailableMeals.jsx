e5d74aa3f171e8e09bc926730ae22e19054f2ab2
arindam <arindam.mitra554@gmail.com>
2021-09-15 23:41:02 +0530

    Built the cart component, (dummy mode). Styled the meal items

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Card from './../UI/Card'
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css'
 const DUMMY_MEALS = [
   {
@@ -29,12 +31,12 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
     return (
         <section className={classes.meals}>
-            <ul>
+            <Card>
                 {mealsList}
-            </ul>
+            </Card>
         </section>
     )
 }
