7392a1b65fc50fc5c98c75f6d73daebdadc82ffe
arindam <arindam.mitra554@gmail.com>
2021-09-19 20:36:37 +0530

    Fixed styling of modal

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import Meals from './components/Meals/Meals'
 import Header from './components/Layout/Header'
+import Cart from './components/Cart/Cart'
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
