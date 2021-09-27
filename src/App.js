584cf7eb7475a189922f5ff844d14ea2a0384b7a
arindam <arindam.mitra554@gmail.com>
2021-09-27 19:45:44 +0530

    Creating the cart context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
+import CartProvider from './store/CartProvider'
 import Meals from './components/Meals/Meals'
 import Header from './components/Layout/Header'
 import Cart from './components/Cart/Cart'
 function App() {
   return (
-    <>
+    <CartProvider>
       <Cart />
       <Header />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   )
 }
 
