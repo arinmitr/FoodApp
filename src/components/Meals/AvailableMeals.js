10a7b3b3d7784bdedb35ee35016fd72a84743283
arindam <arindam.mitra554@gmail.com>
2021-10-26 18:45:11 +0530

    Food menu now fetched from backend

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,31 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Card from '../UI/Card'
+import MealItem from './MealItem/MealItem'
+import classes from './AvailableMeals.module.css'
+import { useEffect, useState } from 'react/cjs/react.development'
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([])
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        'https://food-app-794bd-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
+      )
+      const data = await response.json()
+
+      const loadedMeals = []
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        })
+      }
+      setMeals(loadedMeals)
+    }
+    fetchMeals()
+  }, [])
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -38,7 +33,7 @@ const AvailableMeals = () => {
       description={meal.description}
       price={meal.price}
     />
-  ));
+  ))
 
   return (
     <section className={classes.meals}>
@@ -46,7 +41,7 @@ const AvailableMeals = () => {
         <ul>{mealsList}</ul>
       </Card>
     </section>
-  );
-};
+  )
+}
 
-export default AvailableMeals;
+export default AvailableMeals
